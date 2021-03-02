87707a5a4d6e6238c1c56ad4c828c2976e55f906
ladni <ladnik@hotmail.com>
2021-03-02 17:11:21 +0200

    Query fixed.

--- Model/ConfigModelLib/Query/ConfigurationManagerQuery.cs
+++ Model/ConfigModelLib/Query/ConfigurationManagerQuery.cs
@@ -1,29 +1,34 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Xml;
+﻿using System.Xml;
+using GraphQL;
 using GraphQL.Types;
 using GraphQlHelperLib;
 using ConfigModelLib.Type;
+using ConfigModelLib.Models;
 
 namespace ConfigModelLib.Query
 {
     public class ConfigurationManagerQuery : ObjectGraphType
     {
         public ConfigurationManagerQuery() =>
-            Field<ListGraphType<ConfigurationManagerType>>("configurationManager", resolve: context =>
-            {
-                var currDir = Directory.GetCurrentDirectory();
-                XmlDocument xmlDocument = new();
-                xmlDocument.Load("ConfigurationManager.xml");
-                
-                context.SetXmlDocument(xmlDocument);
-
-                List<Models.ConfigurationManager> lstCm = new()
+            Field<ConfigurationManagerType>("configurationManager",
+                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "path" }),
+                resolve: context =>
                 {
-                    new() { BasePath = "xxx" }
-                };
+                    const string defaultFileName = "ConfigurationManager.xml";
+                    var path = context.GetArgument<string>("path");
+                    if (string.IsNullOrEmpty(path))
+                        return "Empty path";
 
-                return lstCm; 
-            });
+                    XmlDocument xmlDocument = new();
+                    var file = !string.IsNullOrEmpty(path) && path.Substring(path.Length - 4)?.ToLower() == ".xml"
+                        ? path
+                        : $@"{path}\{defaultFileName}";
+
+                    xmlDocument.Load(file);
+
+                    context.SetXmlDocument(xmlDocument);
+
+                    return new ConfigurationManager { BasePath = (xmlDocument?.FirstChild as XmlElement).GetAttribute("basePath") };
+                });
     }
 }
